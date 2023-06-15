04965288936d655107fd8f2deb5237983dcce470
iamcjt922 <tejadachristian92@gmail.com>
2023-06-14 18:55:13 -0700

    Fix some bugs

---
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Blog_ASP.NET.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_ASP.NET.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        // Custom Table Names for Identify
+        builder.HasDefaultSchema("Indetify");
+
+        builder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "Users");
+        });
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Roles");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
+    }
+}
